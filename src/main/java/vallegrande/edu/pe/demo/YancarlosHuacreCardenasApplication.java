cbd54b4748a516b32f614242a0cffbc8d8e1f082
Yancarlos Huacre Cardenas <yancarlos.huacre@vallegrande.edu.pe>
2025-08-14 13:57:13 -0500

    Implementing the Hello World Endpoint in Spring Boot

---
+++ src/main/java/vallegrande/edu/pe/demo/YancarlosHuacreCardenasApplication.java
@@ -0,0 +1,13 @@
+package vallegrande.edu.pe.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YancarlosHuacreCardenasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YancarlosHuacreCardenasApplication.class, args);
+	}
+
+}
